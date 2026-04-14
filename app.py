642617087828c32cbb9284c5c530d9ed194c1172
tanvirmohammadsiam-sketch <tanvir.mohammad.siam@g.bracu.ac.bd>
2026-04-14 07:24:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -139,5 +139,5 @@ def upload():
 # -------------------------
 # RUN SERVER
 # -------------------------
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
